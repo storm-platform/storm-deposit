bd1d7cdab7e0b0d0e74db2915916af48695d4ed2
Felipe <efelipecarlos@gmail.com>
2022-02-20 09:52:59 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup_requires = []
 
 install_requires = [
     # Storm dependencies
-    "storm-execution @ git+https://github.com/storm-platform/storm-pipeline@package/rename-0.1",
+    "storm-workflow @ git+https://github.com/storm-platform/storm-workflow@main",
 ]
 
 packages = find_packages()
